acd7dad9720a24ea471d95bc6252827625216813
6704062610062 <s6704062610062@email.kmutnb.ac.th>
2024-11-28 15:16:53 +0700

    ข้อ4

--- lab1minx.c
+++ lab1minx.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
-
 int main() {
 	int n1,n2;
-	
-	scanf("%d %d", &n1,n2);
-	
+	scanf("%d", &n1);
+	scanf("%d", &n2);
 	if(n1 > n2) {
 		int total = n1 - n2;
 		printf("num1 greater than num2 = %d\n", total);
 	} 
 	else {
 		int total = n2 - n1;
+		printf("num2 greater than num1  =%d", total);
 		printf("num2 greater than num1  =%d\n", total);
 	}
 }
