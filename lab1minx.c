22b3d2e658d4f9be95fbf36038468f5f3c0987d4
6704062610062 <s6704062610062@email.kmutnb.ac.th>
2024-11-28 15:02:06 +0700

    ข้อ3

--- lab1minx.c
+++ lab1minx.c
@@ -1,21 +1,16 @@
 #include<stdio.h>
 
 int main() {
-	int n1;
-	int n2;
+	int n1,n2;
 	
-	printf("input n1: ");
-	scanf("%d", &n1);
-	
-	printf("input n2: ");
-	scanf("%d", &n2);
+	scanf("%d %d", &n1,n2);
 	
 	if(n1 > n2) {
 		int total = n1 - n2;
-		printf("num1 greater than num2 = %d", total);
+		printf("num1 greater than num2 = %d\n", total);
 	} 
 	else {
 		int total = n2 - n1;
-		printf("num2 greater than num1  =%d", total);
+		printf("num2 greater than num1  =%d\n", total);
 	}
 }
